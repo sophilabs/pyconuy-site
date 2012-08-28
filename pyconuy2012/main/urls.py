3a942e1487102290c7b1e138e3a003032415ed03
ssassi <ssassi@sophilabs.com>
2012-08-27 22:30:56 -0300

    texts defined

--- pyconuy2012/main/urls.py
+++ pyconuy2012/main/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import *
 urlpatterns = patterns('main.views',
     url(r"^$", 'index', name='index'),
     url(r"^proposal$", 'proposal_add', name='proposal_add'),
+    url(r"^proposal-info$", 'proposal_info', name='proposal_info'),
     url(r"^about$", 'about', name='about'),
     url(r"^proposal-sent$", 'proposal_sent', name='proposal_sent'),
+    url(r"^schedule$", 'schedule', name='schedule'),
 )
